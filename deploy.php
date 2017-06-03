1f2adf8dd902315dc8639984fc657e1befd63fe0
Pratinav Bagla <pb@baglas.net>
2017-06-03 17:08:55 +0530

    Updated deploy.php

--- deploy.php
+++ deploy.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
   </head>
-  <body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+  <body>
     <pre>
       <?php echo htmlentities(trim(shell_exec("~/deploy.sh"))); ?>
     </pre>
