2531e1b436dec257a2fd0be5631209ed3228bb6a
Pratinav Bagla <pb@baglas.net>
2017-06-03 16:50:57 +0530

    Updated deploy script

--- deploy.php
+++ deploy.php
@@ -1,3 +1,25 @@
 <?php
-  shell_exec("~/deploy.sh");
+  $commands = array(
+    'cd ~/dsmun17.com',
+    'git pull',
+    'jekyll build -d ~/public_html'
+  );
+  
+  $output = '';
+  foreach($commands AS $command){
+    $tmp = shell_exec($command);
+    $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+    $output .= htmlentities(trim($tmp)) . "\n";
+  }
 ?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<pre>
+<?php echo $output; ?>
+</pre>
+</body>
+</html>
