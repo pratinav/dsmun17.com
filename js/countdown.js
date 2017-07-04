b3e55fee3a8c9d1d3f6a417b24cfc9737e867291
Shreyas Minocha <sm-git@riseup.net>
2017-07-04 14:31:17 +0530

    Add countdown

---
+++ js/countdown.js
@@ -0,0 +1,42 @@
+var endDate = "August 18, 2017 00:00:00 GMT+0530";
+
+function getRemainingTime(deadline) {
+    var t = Date.parse(deadline) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+
+    return {
+        "total": t,
+        "seconds": seconds,
+        "minutes": minutes,
+        "hours": hours,
+        "days": days
+    }
+}
+
+function initializeCountdown(id, deadline) {
+    var countdown = document.getElementById(id);
+    var daysSpan = countdown.querySelector(".days");
+    var hoursSpan = countdown.querySelector(".hours");
+    var minutesSpan = countdown.querySelector(".minutes");
+    var secondsSpan = countdown.querySelector(".seconds");
+    function updateClock() {
+        var time = getRemainingTime(endDate);
+
+        daysSpan.innerHTML = ("0" + time.days).slice(-2);
+        hoursSpan.innerHTML = ("0" + time.hours).slice(-2);
+        minutesSpan.innerHTML = ("0" + time.minutes).slice(-2);
+        secondsSpan.innerHTML = ("0" + time.seconds).slice(-2);
+
+        if(time.total <= 0){
+            clearInterval(timeInterval);
+        }
+    }
+
+    updateClock(); // run function once at first to avoid delay
+    var timeInterval = setInterval(updateClock, 1000);
+}
+
+initializeCountdown("countdown", endDate);
